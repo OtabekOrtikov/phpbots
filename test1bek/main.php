ed885d2b07344405f7c6bc71c7f43dc495adb634
otabek_ortikov <otabek_ortikov@student.itpu.uz>
2023-11-18 19:11:03 +0500

    6 commit

--- test1bek/main.php
+++ test1bek/main.php
@@ -21,69 +21,170 @@ $msg = $data->message;
 $text = $msg->text;
 $id = $msg->chat->id;
 $mid = $msg->message_id;
+$channelName = "@test1Otabek";
+$channelInfo = json_decode(file_get_contents("https://api.telegram.org/bot" . API_KEY . "/getChat?chat_id={$channelName}"), TRUE);
+$channelID = $channelInfo['result']['id'];
+$channelTitle = $channelInfo['result']['title'];
+$channelUser = $channelInfo['result']['username'];
+$isMember = false;
+
+$bot_api_url = 'https://api.telegram.org/bot' . API_KEY . '/';
+$bot_api_method = 'getChatMember';
+
+// Prepare the request URL
+$request_url = $bot_api_url . $bot_api_method . '?chat_id=' . $channelID . '&user_id=' . $id;
+
+// Send the request to the Telegram Bot API
+$response = file_get_contents($request_url);
+
+// Decode the response JSON
+$dataMem = json_decode($response, true);
+
+// Check if the user is a member of the channel
+if ($dataMem['ok'] && ($dataMem['result']['status'] == 'member' || $dataMem['result']['status'] == 'creator' || $dataMem['result']['status'] == 'administrator')) {
+    $isMember = true;
+    
+} else {
+    $isMember = false;
+}
 
 if ($text == '/start') {
-    bot('sendMessage', [
-        'chat_id' => $id,
-        'text' => "Welcome! Do you want to send a message to the channel?",
-        'parse_mode' => 'html',
-        'reply_markup' => json_encode([
-            'resize_keyboard' => true,
-            'keyboard' => [
-                [['text' => "✍🏻Write a message"]],
-            ],
-        ]),
-    ]);
-}
-
-if ($text == "✍🏻Write a message") {
-    bot('sendMessage', [
-        'chat_id' => $id,
-        'text' => "Write a message containing at least 5 words",
-        'parse_mode' => 'html',
-        'reply_markup' => json_encode([
-            'resize_keyboard' => true,
-            'keyboard' => [
-                [['text' => "↩Back"]],
-            ],
-        ]),
-    ]);
-}
-
-if ($text && $text != "✍🏻Write a message" && $text != "/start" && strpos($text, "/") === false) {
-    $words = file('words.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-    $foundWord = false;
-    foreach ($words as $word) {
-        if (strpos($text, $word) !== false) {
-            bot('sendMessage', [
-                'chat_id' => $id,
-                'text' => 'Watch your language!',
-                'parse_mode' => 'html',
-            ]);
-            $foundWord = true;
-            break;
-        }
-    }
-    if (!$foundWord) {
+    if ($isMember) {
         bot('sendMessage', [
-            'chat_id' => $channelID,
-            'text' => $text,
+            'chat_id' => $id,
+            'text' => "Добро пожаловать! Вы хотите отправить сообщение на канал?",
             'parse_mode' => 'html',
+            'reply_markup' => json_encode([
+                'resize_keyboard' => true,
+                'keyboard' => [
+                    [['text' => "✍🏻Написать сообщение"]],
+                ],
+            ]),
+        ]);
+        
+    } else {
+        bot('sendMessage', [
+            'chat_id' => $id,
+            'text' => 'Чтобы продолжить, вам должны подписаться на наш канал',
+            'parse_mode' => 'html',
+            'reply_markup' => json_encode([
+                'inline_keyboard' => [
+                    [
+                        ['text' => 'Subcribe', 'url' => 'https://t.me/test1Otabek']
+                    ]
+                ],
+            ])
         ]);
     }
 }
 
-if ($text == "↩Back") {
-    bot('sendMessage', [
-        'chat_id' => $id,
-        'text' => 'To leave a message, press the button ✍🏻Write a message',
-        'parse_mode' => 'html',
-        'reply_markup' => json_encode([
-            'resize_keyboard' => true,
-            'keyboard' => [
-                [['text' => "✍🏻Write a message"]],
-            ],
-        ]),
-    ]);
+if ($text == "✍🏻Написать сообщение") {
+    if ($isMember) {
+        bot('sendMessage', [
+            'chat_id' => $id,
+            'text' => "Пожалуйста, напишите сообщение!",
+            'parse_mode' => 'html',
+            'reply_markup' => json_encode([
+                'resize_keyboard' => true,
+                'keyboard' => [
+                    [['text' => "↩Назад"]],
+                ],
+            ]),
+        ]);
+        
+    } else {
+        bot('sendMessage', [
+            'chat_id' => $id,
+            'text' => 'Чтобы продолжить, вам должны подписаться на наш канал',
+            'parse_mode' => 'html',
+            'reply_markup' => json_encode([
+                'inline_keyboard' => [
+                    [
+                        ['text' => 'Subcribe', 'url' => 'https://t.me/test1Otabek']
+                    ]
+                ],
+            ])
+        ]);
+    }
+}
+if ($text && $text != "✍🏻Написать сообщение" && $text != "/start" && strpos($text, "/") == false && $text != "↩Назад") {
+    if ($isMember) {
+        $words = file('words.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+        $foundWord = false;
+        foreach ($words as $word) {
+            if (strpos($text, $word) !== false || strpos($text, "@") !== false || strpos($text, "t.me") !== false || strpos($text, "http") !== false) {
+                bot('sendMessage', [
+                    'chat_id' => $id,
+                    'text' => 'Вы использовали запрещенное слово или включили ссылку!',
+                    'parse_mode' => 'html',
+                ]);
+                $foundWord = true;
+                break;
+            }
+        }
+        if (!$foundWord) {
+            bot('sendMessage', [
+                'chat_id' => $channelID,
+                'text' => $text,
+                'parse_mode' => 'html',
+            ]);
+            bot('sendMessage', [
+                'chat_id' => $id,
+                'text' => 'Ваше сообщение было отправлено на адрес '.$channelName.'
+Ваш текст:'.$text,
+                'parse_mode' => 'markdown',
+                'reply_markup' => json_encode([
+                    'resize_keyboard' => true,
+                    'keyboard' => [
+                        [['text' => "✍🏻Написать сообщение"]],
+                    ],
+                ]),
+            ]);
+        }  
+    } else {
+        bot('sendMessage', [
+            'chat_id' => $id,
+            'text' => 'Чтобы продолжить, вам должны подписаться на наш канал',
+            'parse_mode' => 'html',
+            'reply_markup' => json_encode([
+                'inline_keyboard' => [
+                    [
+                        ['text' => 'Subcribe', 'url' => 'https://t.me/test1Otabek']
+                    ]
+                ],
+            ])
+        ]);
+    }
+}
+
+
+
+if ($text == "↩Назад") {
+    if ($isMember) {
+        bot('sendMessage', [
+            'chat_id' => $id,
+            'text' => 'Чтобы оставить сообщение, нажмите кнопку "✍🏻Написать сообщение"',
+            'parse_mode' => 'html',
+            'reply_markup' => json_encode([
+                'resize_keyboard' => true,
+                'keyboard' => [
+                    [['text' => "✍🏻Написать сообщение"]],
+                ],
+            ]),
+        ]);
+    } else {
+        bot('sendMessage', [
+            'chat_id' => $id,
+            'text' => 'Чтобы продолжить, вам должны подписаться на наш канал',
+            'parse_mode' => 'html',
+            'reply_markup' => json_encode([
+                'inline_keyboard' => [
+                    [
+                        ['text' => 'Subcribe', 'url' => 'https://t.me/test1Otabek']
+                    ]
+                ],
+            ])
+        ]);
+    }
 }
 ?>
